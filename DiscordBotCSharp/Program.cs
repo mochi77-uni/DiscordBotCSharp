978e743eed29775caa77b170459bd4c8b814ff8f
mochi77-uni <mochi77.uni@gmail.com>
2025-06-14 18:18:47 +0800

    Add initial bot setup with token management and basic login functionality

--- DiscordBotCSharp/Program.cs
+++ DiscordBotCSharp/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using Discord;
+using Discord.WebSocket;    
 
-Console.WriteLine("Hello, World!");
+namespace DiscordBotCSharp;
+
+internal static class Program
+{
+    private static DiscordSocketClient? _client;
+    
+    public static async Task Main()
+    {
+        var token = "";
+        try
+        {
+            const string tokenPath = "token.txt";
+            token = await File.ReadAllTextAsync(tokenPath);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Failed to read token.txt");
+            Console.WriteLine(e);
+            throw;
+        }
+        
+        _client = new DiscordSocketClient();
+
+        _client.Log += message =>
+        {
+            Console.WriteLine(message);
+            return Task.CompletedTask;
+        };
+        
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        
+        await Task.Delay(-1);
+    } 
+}
